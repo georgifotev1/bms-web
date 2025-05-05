b76000e7a84fe0fc69a77cfaf89b3b5d60b326e9
Fotev Gerogi <georgifotev@protonmail.com>
2025-05-05 13:18:53 +0300

    added typography helpers

--- src/app/routes/landing/root.tsx
+++ src/app/routes/landing/root.tsx
@@ -1,3 +1,4 @@
+import { H1, Lead } from '@/components/typography';
 import { buttonVariants } from '@/components/ui/button';
 import { env } from '@/config/env';
 import { useTranslation } from 'react-i18next';
@@ -7,12 +8,12 @@ export const LandinRoute = () => {
     return (
         <section className='bg-white dark:bg-gray-900'>
             <div className='py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12'>
-                <h1 className='mb-4 font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
+                <H1 className='mb-4 font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
                     {t('landing.welcomeHeading')}
-                </h1>
-                <p className='mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400'>
+                </H1>
+                <Lead className='mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400'>
                     {t('landing.welcomeDescription')}
-                </p>
+                </Lead>
                 <div className='flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4'>
                     <a
                         className={buttonVariants({ variant: 'default' })}
---
+++ src/components/typography.tsx
@@ -0,0 +1,150 @@
+import { cn } from '@/lib/utils';
+
+interface TypographyProps extends React.HTMLAttributes<HTMLElement> {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export const H1 = ({ children, className, ...props }: TypographyProps) => {
+    return (
+        <h1
+            className={cn(
+                'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </h1>
+    );
+};
+
+export const H2 = ({ children, className, ...props }: TypographyProps) => {
+    return (
+        <h2
+            className={cn(
+                'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </h2>
+    );
+};
+
+export const H3 = ({ children, className, ...props }: TypographyProps) => {
+    return (
+        <h3
+            className={cn(
+                'scroll-m-20 text-2xl font-semibold tracking-tight',
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </h3>
+    );
+};
+
+export const H4 = ({ children, className, ...props }: TypographyProps) => {
+    return (
+        <h4
+            className={cn(
+                'scroll-m-20 text-xl font-semibold tracking-tight',
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </h4>
+    );
+};
+
+export const Paragraph = ({
+    children,
+    className,
+    ...props
+}: TypographyProps) => {
+    return (
+        <p
+            className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}
+            {...props}
+        >
+            {children}
+        </p>
+    );
+};
+
+export const Blockquote = ({
+    children,
+    className,
+    ...props
+}: TypographyProps) => {
+    return (
+        <blockquote
+            className={cn(
+                'mt-6 border-l-2 border-slate-300 pl-6 italic text-slate-800 dark:border-slate-600 dark:text-slate-200',
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </blockquote>
+    );
+};
+
+export const Lead = ({ children, className, ...props }: TypographyProps) => {
+    return (
+        <p
+            className={cn(
+                'text-xl text-slate-700 dark:text-slate-300',
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </p>
+    );
+};
+
+export const LargeText = ({
+    children,
+    className,
+    ...props
+}: TypographyProps) => {
+    return (
+        <div className={cn('text-lg font-semibold', className)} {...props}>
+            {children}
+        </div>
+    );
+};
+
+export const SmallText = ({
+    children,
+    className,
+    ...props
+}: TypographyProps) => {
+    return (
+        <small
+            className={cn('text-sm font-medium leading-none', className)}
+            {...props}
+        >
+            {children}
+        </small>
+    );
+};
+
+export const Muted = ({ children, className, ...props }: TypographyProps) => {
+    return (
+        <p
+            className={cn(
+                'text-sm text-slate-500 dark:text-slate-400',
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </p>
+    );
+};
