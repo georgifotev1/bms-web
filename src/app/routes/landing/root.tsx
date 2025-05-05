244a31a195d14a6150140225ec8d0c548dbc6092
Fotev Gerogi <georgifotev@protonmail.com>
2025-05-05 12:57:05 +0300

    add translations

--- src/app/routes/landing/root.tsx
+++ src/app/routes/landing/root.tsx
@@ -1,24 +1,24 @@
 import { buttonVariants } from '@/components/ui/button';
 import { env } from '@/config/env';
+import { useTranslation } from 'react-i18next';
 
 export const LandinRoute = () => {
+    const { t } = useTranslation();
     return (
         <section className='bg-white dark:bg-gray-900'>
             <div className='py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12'>
                 <h1 className='mb-4 font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
-                    Booking Management System
+                    {t('landing.welcomeHeading')}
                 </h1>
                 <p className='mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400'>
-                    Your all-in-one platform for seamless booking management.
-                    Simplify scheduling, maximize efficiency, and enhance your
-                    customer experience with our powerful booking solution.
+                    {t('landing.welcomeDescription')}
                 </p>
                 <div className='flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4'>
                     <a
                         className={buttonVariants({ variant: 'default' })}
                         href={`http://app.${env.APP_URL}`}
                     >
-                        Get started
+                        {t('landing.getStarted')}
                     </a>
                 </div>
             </div>
