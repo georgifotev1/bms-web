1f6faf4de0e26470e1802c6dc57e873e5d1c44ef
Fotev Georgi <georgifotev@protonmail.com>
2025-07-20 22:37:18 +0300

    fix services header

--- src/features/services/components/services-list.tsx
+++ src/features/services/components/services-list.tsx
@@ -13,64 +13,59 @@ export const ServicesList = () => {
     if (isLoading) return <LoadingScreen />;
     if (!services.data) return;
     return (
-        <section className='py-8'>
-            <div className='container px-0 md:px-12 mx-auto'>
-                <div className='flex justify-between'>
-                    <H3>Services ({services.data.length})</H3>
-                    <ButtonLink
-                        to={paths.app.services.new}
-                        variant='default'
-                        size='icon'
+        <section className='container md:px-12 mx-auto'>
+            <div className='flex items-center justify-between h-14 sticky bg-background top-0 z-10 lg:px-10  border-b border-solid border-tertiary'>
+                <H3>Services ({services.data.length})</H3>
+                <ButtonLink
+                    to={paths.app.services.new}
+                    variant='default'
+                    size='icon'
+                >
+                    <Plus />
+                </ButtonLink>
+            </div>
+
+            <div className='flex flex-col gap-2 my-8'>
+                {services.data.map(service => (
+                    <div
+                        key={service.id}
+                        className='flex items-center gap-4 py-2 px-4 justify-between border rounded-sm cursor-pointer hover:bg-accent/50'
+                        onClick={() => navigate(`/services/${service.id}`)}
                     >
-                        <Plus />
-                    </ButtonLink>
-                </div>
-
-                <div className='flex flex-col gap-2 mt-8'>
-                    {services.data.map(service => (
-                        <div
-                            key={service.id}
-                            className='flex items-center gap-4 py-2 px-4 justify-between border rounded-sm cursor-pointer hover:bg-accent/50'
-                            onClick={() => navigate(`/services/${service.id}`)}
-                        >
-                            {service.imageUrl ? (
-                                <img
-                                    src={service.imageUrl}
-                                    width={50}
-                                    alt={service.title}
-                                    className='shrink-0 rounded-sm'
-                                    loading='lazy'
-                                />
-                            ) : (
-                                <div className='w-[50px] h-[50px] bg-accent flex items-center justify-center rounded-sm shrink-0'>
-                                    <Image />
-                                </div>
-                            )}
-
-                            <div className='flex flex-1 flex-col gap-2'>
-                                <SmallText>{service.title}</SmallText>
-                                <div className='flex gap-1'>
-                                    <SmallTextMuted>
-                                        {service.duration} min
-                                    </SmallTextMuted>
-                                    <SmallTextMuted>-</SmallTextMuted>
-                                    <SmallTextMuted>
-                                        $
-                                        {service.cost != ''
-                                            ? service.cost
-                                            : '0'}
-                                    </SmallTextMuted>
-                                </div>
+                        {service.imageUrl ? (
+                            <img
+                                src={service.imageUrl}
+                                width={50}
+                                alt={service.title}
+                                className='shrink-0 rounded-sm'
+                                loading='lazy'
+                            />
+                        ) : (
+                            <div className='w-[50px] h-[50px] bg-accent flex items-center justify-center rounded-sm shrink-0'>
+                                <Image />
                             </div>
-                            <div>
-                                <ProvidersAvatars
-                                    providers={service.providers}
-                                    users={users.data}
-                                />
+                        )}
+
+                        <div className='flex flex-1 flex-col gap-2'>
+                            <SmallText>{service.title}</SmallText>
+                            <div className='flex gap-1'>
+                                <SmallTextMuted>
+                                    {service.duration} min
+                                </SmallTextMuted>
+                                <SmallTextMuted>-</SmallTextMuted>
+                                <SmallTextMuted>
+                                    ${service.cost != '' ? service.cost : '0'}
+                                </SmallTextMuted>
                             </div>
                         </div>
-                    ))}
-                </div>
+                        <div>
+                            <ProvidersAvatars
+                                providers={service.providers}
+                                users={users.data}
+                            />
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
     );
